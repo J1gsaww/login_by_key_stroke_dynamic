2221cbd3bbc8ebc8a63fb40105ffaafffc6cbd93
Kittipat Arpanon <kittipat.arp@student.mahidol.ac.th>
2023-04-11 14:58:45 +0700

    update register screen for keyboard layout and hide password

--- lib/screens/registerScreen.dart
+++ lib/screens/registerScreen.dart
@@ -10,6 +10,7 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,34 +20,39 @@ class _RegisterScreenState extends State<RegisterScreen> {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
+            key: formKey,
             child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                "E-mail",
-                style: TextStyle(fontSize: 20),
-              ),
-              TextFormField(),
-              const SizedBox(height: 15),
-              const Text(
-                "Password",
-                style: TextStyle(fontSize: 20),
-              ),
-              TextFormField(),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  child: const Text(
-                    "Registered",
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "E-mail",
                     style: TextStyle(fontSize: 20),
                   ),
-                  onPressed: () {},
-                ),
-              )
-            ],
-          ),
-        )),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    "Password",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      child: const Text(
+                        "Registered",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            )),
       ),
     );
   }
