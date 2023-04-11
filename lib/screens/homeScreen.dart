ab22de854130817f07f7984ff3f6da3391daa6ed
Kittipat Arpanon <kittipat.arp@student.mahidol.ac.th>
2023-04-11 13:47:43 +0700

    create new homescreen

---
+++ lib/screens/homeScreen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Register/Login"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 50, 10, 0),
+        child: Column(
+          children: [
+            Image.asset("assets/images/logo.png", height: 100, width: 100),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.add),
+                  label: const Text(
+                    "Register",
+                    style: TextStyle(fontSize: 20),
+                  )),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.login),
+                  label: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20),
+                  )),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
